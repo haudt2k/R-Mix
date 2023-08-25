564302ea4cc8bc6fca200fb939eba9520685a615
ZiSaii <hauaini@gmail.com>
2023-08-25 09:59:04 +0700

    hau

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $(".image-slider").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      draggable: false,
+      autoplay: true,
+      autoplayspeed: 2000,
+      arrows: true,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-solid fa-angle-left" style="color: #ffffff;"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-angle-right" style="color: #ffffff;"></i></button>`,
+      dots: true,
+      responsive: [
+        {        
+          breakpoint: 989,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1.5,
+          },
+        }
+      ],
+    });
+  });
+
+//nav 
+const hamburger =  document.querySelector('.hamburger');
+const menu =  document.querySelector('.menu');
+hamburger.addEventListener('click', function(){
+    this.classList.toggle('open');
+    menu.classList.toggle('open');
+})
